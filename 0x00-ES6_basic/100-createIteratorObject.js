43ba1ac15a92ff724bf26cb4f7a384078167fad9
Adjamkwalim Akum-Yong <akumyongt27@gmail.com>
2023-09-25 19:32:25 +0000

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,36 +1,14 @@
-export default function createEmployeesObject(department, employees) {
-  const employeesObj = {};
-  employeesObj[department] = employees;
-  return employeesObj;
+export default function createIteratorObject(report) {
+  return {
+    [Symbol.iterator]: function* () {
+      for (const department in report) {
+        for (const employee of report[department]) {
+          yield {
+            department,
+            employee,
+          };
+        }
+      }
+    },
+  };
 }
-
-export default function createReportObject(employees) {
-  const report = {};
-  for (const department in employees) {
-    report[department] = employees[department];
-  }
-  return report;
-}
-
-export default function* createIteratorObject(report) {
-  for (const department in report) {
-    const employees = report[department];
-    for (const employee of employees) {
-      yield employee;
-    }
-  }
-}
-
-const employees = {
-  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-  ...createEmployeesObject('marketing', ['Sylvie'])
-};
-
-const report = createReportObject(employees);
-
-const reportWithIterator = createIteratorObject(report);
-
-for (const item of reportWithIterator) {
-  console.log(item);
-}
-
